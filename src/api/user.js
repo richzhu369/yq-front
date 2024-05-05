97f3fef12203833c6cf00dc368de99e74f6b6889
Benjamin <benjamin@BenjamindeMac-mini.local>
2024-05-05 23:27:41 +0800

    first commit

---
+++ src/api/user.js
@@ -0,0 +1,48 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: 'http://127.0.0.1:8080/api/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: 'http://127.0.0.1:8080/api/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: 'http://127.0.0.1:8080/api/user/logout',
+    method: 'post'
+  })
+}
+
+export function create_user(data) {
+  return request({
+    url: 'http://127.0.0.1:8080/api/user/new',
+    method: 'post',
+    data
+  })
+}
+
+export function delete_user(data) {
+  return request({
+    url: 'http://127.0.0.1:8080/api/user/delete',
+    method: 'delete',
+    data
+  })
+}
+
+export function update_user(data) {
+  return request({
+    url: 'http://127.0.0.1:8080/api/user/update',
+    method: 'put',
+    data
+  })
+}
