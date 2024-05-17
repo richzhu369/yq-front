96e8346d05083995cad3f039eb4895e682a31dac
Benjamin <benjamin@BenjamindeMac-mini.local>
2024-05-17 23:27:52 +0800

    修改一个接口问题

--- src/api/article.js
+++ src/api/article.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function fetchList(query) {
   return request({
-    url: 'http://127.0.0.1:8080/api/ingress/whitelistLog',
+    url: '/api/ingress/whitelistLog',
     method: 'get',
     params: query
   })
