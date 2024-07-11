543a9576ba4aa9802a0978e01ed10e02a128387c
Benjamin <benjamin@BenjamindeMac-mini.local>
2024-07-11 23:00:35 +0800

    完成前端域名的增删功能

--- src/api/merchantManagement.js
+++ src/api/merchantManagement.js
@@ -33,3 +33,27 @@ export function fetchLogList(query) {
     params: query
   })
 }
+
+export function bindFrontendDomain(data){
+  return request({
+    url: '/api/merchant/bindFrontendDomain',
+    method: 'post',
+    data: data
+  })
+}
+
+export function getFrontendDomain(query){
+  return request({
+    url: '/api/merchant/getFrontendDomain',
+    method: 'get',
+    params: query
+  })
+}
+
+export function deleteFrontendDomain(data){
+  return request({
+    url: '/api/merchant/deleteFrontendDomain',
+    method: 'delete',
+    data: data
+  })
+}
--- src/views/table/merchantManagement.vue
+++ src/views/table/merchantManagement.vue
@@ -23,13 +23,28 @@
     >
       <el-table-column label="站点名称" min-width="30px">
         <template slot-scope="{row}">
-          <el-button v-if="row.Status === 'process'" type="text" icon="el-icon-loading" @click="getCreateStatus(row.MerchantName)">
+          <el-button
+            v-if="row.Status === 'process'"
+            type="text"
+            icon="el-icon-loading"
+            @click="getCreateStatus(row.MerchantName)"
+          >
             {{ row.MerchantName }}
           </el-button>
-          <el-button v-if="row.Status === 'failed'" type="text" icon="el-icon-close" @click="getCreateStatus(row.MerchantName)">
+          <el-button
+            v-if="row.Status === 'failed'"
+            type="text"
+            icon="el-icon-close"
+            @click="getCreateStatus(row.MerchantName)"
+          >
             {{ row.MerchantName }}
           </el-button>
-          <el-button v-if="row.Status === 'done'" type="text" icon="el-icon-check" @click="getCreateStatus(row.MerchantName)">
+          <el-button
+            v-if="row.Status === 'done'"
+            type="text"
+            icon="el-icon-check"
+            @click="getCreateStatus(row.MerchantName)"
+          >
             {{ row.MerchantName }}
           </el-button>
         </template>
@@ -41,7 +56,7 @@
       </el-table-column>
       <el-table-column label="前端域名" min-width="80px">
         <template slot-scope="{row}">
-          <span>{{ row.ip }}</span>
+          <span>{{ row.FrontendDomain }}</span>
         </template>
       </el-table-column>
       <el-table-column label="财务域名" min-width="80px">
@@ -62,19 +77,19 @@
 
       <el-table-column label="操作" align="center" min-width="150px" class-name="small-padding fixed-width">
         <template slot-scope="{row}">
-          <el-button size="mini" @click="bindDomain(row.ip)">
-            绑定域名
+          <el-button size="mini" @click="bindDomain(row.MerchantName)">
+            前端域名
           </el-button>
-          <el-button size="mini" type="primary" @click="IPwhiteList(row.ip)">
+          <el-button size="mini" type="primary" @click="IPwhiteList(row.MerchantName)">
             IP加白
           </el-button>
-          <el-button size="mini" type="success" @click="ContinentWhite(row.ip)">
+          <el-button size="mini" type="success" @click="ContinentWhite(row.MerchantName)">
             大洲加白
           </el-button>
-          <el-button size="mini" type="warning" @click="CountryWhite(row.ip)">
+          <el-button size="mini" type="warning" @click="CountryWhite(row.MerchantName)">
             国家加白
           </el-button>
-          <el-button size="mini" type="danger" @click="deleteMerchant(row.ip)">
+          <el-button size="mini" type="danger" @click="deleteMerchant(row.MerchantName)">
             关站
           </el-button>
         </template>
@@ -94,6 +109,40 @@
         <el-button type="primary" @click="handleCreateMerchant">确 定</el-button>
       </div>
     </el-dialog>
+
+    <el-dialog :title="`${form.merchantName} 前端域名`" :visible.sync="bindDomainVisible" @close="bindDomainDialogClose">
+      <div class="filter-container">
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="bindNewDomainDialogVisible = true" @close="bindNewDomainDialogVisible = false">
+          增加域名
+        </el-button>
+      </div>
+      <el-table :key="tableKey" :data="merchantDomainList" border fit highlight-current-row style="width: 100%;">
+        <el-table-column label="域名" prop="domain" align="center" min-width="100px">
+          <template slot-scope="{row}">
+            <span>{{ row.domain }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="Actions" align="center" min-width="30px">
+          <template slot-scope="{row,$index}">
+            <el-button size="mini" type="danger" @click="removeDomain(row.domain,form.merchantName,$index)">
+              Delete
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-dialog>
+
+    <el-dialog :title="`${form.merchantName} 绑定域名`" :visible.sync="bindNewDomainDialogVisible" width="20%" @close="NewDomainDialogClose">
+      <el-form ref="newFrontDomain" :model="newFrontDomain" status-icon :rules="domainRules" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="前端域名" prop="domain">
+          <el-input v-model="newFrontDomain.domain" placeholder="请输入新域名:" autocomplete="off" name="newFrontDomain" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="success" @click="runBind(newFrontDomain.domain)">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
     <el-drawer
       :title="form.merchantName"
       :visible.sync="createProgressVisible"
@@ -120,9 +169,16 @@
 </template>
 
 <script>
-import { createMerchant, createProgress, fetchList } from '@/api/merchantManagement'
+import {
+  bindFrontendDomain,
+  createMerchant,
+  createProgress,
+  deleteFrontendDomain,
+  fetchList,
+  getFrontendDomain
+} from '@/api/merchantManagement'
 import waves from '@/directive/waves' // waves directive
-import Pagination from '@/components/Pagination' // secondary package based on el-pagination
+import Pagination from '@/components/Pagination'
 export default {
   name: 'ComplexTable',
   components: { Pagination },
@@ -138,9 +194,22 @@ export default {
     }
   },
   data() {
+    const isURL = require('validator/lib/isURL')
+    const validateFrontDomain = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('域名不可以为空'))
+      } else if (/[A-Z]/.test(value)) {
+        callback(new Error('域名不能包含大写字母'))
+      } else if (!isURL(value, { require_tld: true })) {
+        callback(new Error('域名格式不正确.'))
+      } else {
+        callback()
+      }
+    }
     return {
       tableKey: 0,
       list: null,
+      merchantDomainList: null,
       total: 0,
       listLoading: true,
       activities: [],
@@ -169,11 +238,21 @@ export default {
       },
       form: {
         merchantName: '',
-        merchantCode: ''
+        merchantCode: '',
+        frontendDomain: ''
       },
       createMerchantVisible: false,
       createProgressVisible: false,
-      MerchantName: ''
+      bindNewDomainDialogVisible: false,
+      bindDomainVisible: false,
+      MerchantName: '',
+      bindDomains: null,
+      newFrontDomain: {
+        domain: ''
+      },
+      domainRules: {
+        domain: [{ required: true, trigger: 'blur', validator: validateFrontDomain }]
+      }
     }
   },
   created() {
@@ -191,6 +270,54 @@ export default {
     cleanCreateForm() {
       this.form.merchantName = ''
       this.form.merchantCode = ''
+    },
+    bindDomainDialogClose() {
+      this.merchantDomainList = ''
+      this.form.frontendDomain = ''
+      this.form.merchantName = ''
+      this.getList()
+    },
+    NewDomainDialogClose() {
+      this.newFrontDomain.domain = ''
+    },
+    bindDomain(data) {
+      this.form.merchantName = data
+      getFrontendDomain(this.form).then(response => {
+        if (response.data.items.length === 0) {
+          this.$message.info('当前商户，未绑定域名')
+          this.bindNewDomainDialogVisible = true
+        } else {
+          this.merchantDomainList = response.data.items
+          this.bindDomainVisible = true
+          // const split = response.data.items.split(',')
+        }
+      }).catch((error) => {
+        console.log(error)
+        this.$message.error(error)
+      })
+    },
+    runBind(domain) {
+      this.$refs.newFrontDomain.validate(valid => {
+        if (valid) {
+          const formData = new FormData()
+          formData.append('merchantName', this.form.merchantName)
+          formData.append('frontendDomain', domain)
+          bindFrontendDomain(formData)
+            .then(res => {
+              this.$message.success('绑定域名成功')
+              this.bindNewDomainDialogVisible = false
+              this.bindDomainVisible = false
+              this.getList()
+              this.newFrontDomain.domain = ''
+            })
+            .catch(error => {
+              this.$message.error(error.msg)
+              console.log(error)
+            })
+        } else {
+          console.log('表单验证不通过')
+        }
+      })
     },
     handleCreateMerchant() {
       const formData = new FormData()
@@ -255,6 +382,19 @@ export default {
     },
     deleteMerchant() {
       this.$message.info('这个功能开发中。')
+    },
+    removeDomain(domain, merchantName, index) {
+      const formData = new FormData()
+      formData.append('merchantName', merchantName)
+      formData.append('deleteDomain', domain)
+
+      deleteFrontendDomain(formData).then((res) => {
+        this.merchantDomainList.splice(index, 1)
+        this.$message.success('前端域名删除成功')
+      }).catch((error) => {
+        this.$message.error('前端域名删除失败:' + error)
+        console.log(error)
+      })
     }
   }
 }
